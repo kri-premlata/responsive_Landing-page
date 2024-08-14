4b61d252f2281497f4c736cd595da198a63ecdb8
Premlata <premlata.kumari@mindtree.com>
2024-08-14 17:35:45 +0530

    responsive page

---
+++ script.js
@@ -0,0 +1,41 @@
+// Responsive website using HTML & CSS 
+
+//1. * Units
+// px :- It is fixed  unit in CSS. 1 px means that the element will be displayed at 1 pixel of width or height.
+// % :- It works on basis of percentage of its parents. (relative to the parent element)
+// vw:- viewPort width - it works on width according to the width of the screen.
+// vh :- viewPort height - it works on height according to the height of the screen.
+// vmax:- viewPort maximum - It works accornding to to  the largest between vw and vh.
+// vmin :- viewPort minimum - It works according to the smallest value among vw,vh.
+// em :-  It is used for font size. The font-size will be relative to the current font size or parent size. 
+// rem :- root em - It is also used for font size but it is based on root font size not on parent size.
+//      -  Root em means it works according to the Screen size.
+// ex : Extends - It is a unit used in font-size property .It represents the x-height of the current fontsize.
+
+
+// flex-wrap:-(wrap or no wrap is a property for wrapping the element) 
+//           - It allows us to make our container wrap its items if there isn't enough space in the main axis.
+//           -  It  is a way to create responsiveness in our web pages by adjusting properties like font sizes, margin.
+//           - It always write in the parent element to make child responsive , It wrapped the child element in its parent.
+
+
+// **CSS Media Queries:-
+//2. @media query:- It is used in responsive web designing which allows us to make our webpage look good on
+//                 different devices with different screen.
+//  Syntax: @media (max-width:600px){.....properties}.
+
+// Key points to keep in mind to make website Responsive.
+// i) Use Percentages instead of Pixels.
+// ii) Make use of Flexbox or Grid layouts.
+// iii) Avoid fixed units like px, pt etc.
+// iv) Use media queries appropriately.
+// v) Using Viewport Units like vw, vh, vmin, vmax.
+
+// i) CSS Flexbox
+// ii) CSS Units
+// iii) Responsive Typography
+// iv) Mobile-First Approach
+// v)  Flexible Images & Media
+// vi) last one practice only required
+
+
